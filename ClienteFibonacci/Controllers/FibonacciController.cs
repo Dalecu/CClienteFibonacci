a6faedc41f8de03046126f759fc4a201b3f67a49
Dalecu <hollowsidex@gmail.com>
2019-04-04 13:18:07 +0200

    Cliente Fibonacci limpio

---
+++ ClienteFibonacci/Controllers/FibonacciController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ClienteFibonacci.Controllers
+{
+    public class FibonacciController : Controller
+    {
+        // GET: Fibonacci
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // POST: Fibonacci
+        [HttpPost]
+        public ActionResult Index(int num)
+        {
+            int[] FibonacciArray;
+            ServicioFibonacci.ServicioFibonacciClient client = new ServicioFibonacci.ServicioFibonacciClient("BasicHttpBinding_IServicioFibonacci");
+            FibonacciArray = client.GetNumerosFibonacci(num);
+            return View(FibonacciArray);
+        }
+    }
+}
